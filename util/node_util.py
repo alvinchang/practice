a83d1c4630855686f21afca642a037673141b266
Alvin <alvinchang2@gmail.com>
2017-11-09 16:05:27 -0800

    Introduce notion of equality for the abstract base class Node.

--- util/node_util.py
+++ util/node_util.py
@@ -14,8 +14,20 @@ class Node:
         """
         self._name = name
 
+    def __eq__(self, other):
+        if isinstance(self, type(other)):
+            return self.__dict__ == other.__dict__
+        return NotImplemented
+
+    def __ne__(self, other):
+        eq = self.__eq__(other)
+        if eq is not NotImplemented:
+            return not self.__eq__(other)
+        return NotImplemented
+
+    def __hash__(self):
+        return tuple(sorted(self.__dict__.items()))
+
     @property
     def name(self):
         return self._name
-
-
