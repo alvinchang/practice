02080682720dd6d82c8a3b90cb1507bb06b913e0
Alvin <alvinchang2@gmail.com>
2017-11-14 17:30:47 -0800

    Change ListMap to use the new KV Pairs, which will provide easy access to the key, and values and added clarity over tuples.

--- maps/maps.py
+++ maps/maps.py
@@ -1,4 +1,5 @@
-from util.map_util import Map, ValueMissingError
+from linkedlists.linkedlists import SinglyLinkedList
+from util.map_util import Map, ValueMissingError, KVPair
 
 
 class ListMap(Map):
@@ -12,35 +13,53 @@ class ListMap(Map):
 
     def get(self, key):
         key_hash = self.hash(key)
-        for (chained_key, chained_value) in self._chain_map[key_hash]:
-            if chained_key == key:
-                return chained_value
+        for _kv_pair in self._chain_map[key_hash]:
+            if _kv_pair.key == key:
+                return _kv_pair.value
         return None
 
     def put(self, key, value):
         key_hash = self.hash(key)
-        kv_pair = (key, value)
+        kv_pair = KVPair(key, value)
         # If the value already exists, update it.
-        for _idx, (chained_key, chained_value) in enumerate(self._chain_map[key_hash]):
-            if chained_key == key:
+        for _idx, _kv_pair in enumerate(self._chain_map[key_hash]):
+            if _kv_pair.key == key:
                 self._chain_map[key_hash][_idx] = kv_pair
         # Otherwise add it to the end of the list.
         self._chain_map[key_hash].append(kv_pair)
 
     def _remove_key(self, key):
         key_hash = self.hash(key)
-        for _idx, (chained_key, chained_value) in enumerate(self._chain_map[key_hash]):
-            if chained_key == key:
+        for _idx, _kv_pair in enumerate(self._chain_map[key_hash]):
+            if _kv_pair.key == key:
                 self._chain_map[key_hash].pop(_idx)
         return None
 
     def remove(self, key):
         key_hash = self.hash(key)
-        for _idx, (chained_key, chained_value) in enumerate(self._chain_map[key_hash]):
-            if chained_key == key:
+        for _idx, _kv_pair in enumerate(self._chain_map[key_hash]):
+            if _kv_pair.key == key:
                 self._chain_map[key_hash].pop(_idx)
                 return
         raise ValueMissingError()
+
+
+class LinkedListMap(Map):
+    """
+    Implementation of a Map backed by a linked list.
+    """
+
+    def __init__(self, size=31):
+        super(LinkedListMap, self).__init__(SinglyLinkedList, size)
+
+    def get(self, key):
+        pass
+
+    def put(self, key, value):
+        pass
+
+    def remove(self, key):
+        pass
 
 
 
