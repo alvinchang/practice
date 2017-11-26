01c436f6bde11b6c24b1158653316a9d1eb7e1f4
Alvin <alvinchang2@gmail.com>
2017-11-25 23:21:20 -0800

    Refactor permutations of a string to be more meaningful, mainly due to the difference with the upcoming permutations of a list, as string doesnt support item assignment like a list - fix name oops.

--- test/discrete/test_permutations.py
+++ test/discrete/test_permutations.py
@@ -1,20 +1,20 @@
 import unittest
 from itertools import permutations
 
-from discrete.permutation import generate_permutations_list
+from discrete.permutation import generate_permutations_string
 
 
 class TestPermutations(unittest.TestCase):
 
     def test_permutations_basic(self):
         basic_string = "abcdef"
-        actual_perms = set(generate_permutations_list(basic_string))
+        actual_perms = set(generate_permutations_string(basic_string))
         expected_perms = set("".join(_p) for _p in permutations(basic_string))
         self.assertTrue(actual_perms == expected_perms)
 
     def test_permutations_empty(self):
         empty_string = " "
-        actual_perms = set(generate_permutations_list(empty_string))
+        actual_perms = set(generate_permutations_string(empty_string))
         expected_perms = set("".join(_p) for _p in permutations(empty_string))
         self.assertTrue(actual_perms == expected_perms)
 
