4cfd88e0009e82dc5b14b50df2775218cd331ad1
Alvin <alvinchang2@gmail.com>
2017-11-30 21:47:56 -0800

    Matrix Spiral and some unit tests.

---
+++ misc/print_matrix_spiral.py
@@ -0,0 +1,67 @@
+
+
+def matrix_spiral(matrix):
+    """
+    Returns the matrix in a spiral format in a list.
+
+    [
+     [ 1, 2, 3 ]
+     [ 4, 5, 6 ]
+     [ 7, 8, 9 ]
+                ]
+
+     => 1, 2, 3, 6, 9, 8, 7, 4, 5
+
+    """
+
+    r_end = len(matrix) - 1
+    c_end = len(matrix[0]) - 1
+
+    r_start = 0
+    c_start = 0
+
+    result = []
+
+    while r_start <= r_end and c_start <= c_end:
+
+        # Left-To-Right
+        for _ in xrange(c_start, c_end+1):
+            result.append(matrix[r_start][_])
+
+        # Increment row index
+        r_start += 1
+
+        # Top-To-Bottom
+        for _ in xrange(r_start, r_end+1):
+            result.append(matrix[_][c_end])
+
+        # Decrement column length
+        c_end -= 1
+
+        # Right-To-Left
+        for _ in xrange(c_end, c_start-1, -1):
+            result.append(matrix[r_end][_])
+
+        # Decrement row length
+        r_end -= 1
+
+        # Bottom-To-Top
+        for _ in xrange(r_end, r_start-1, -1):
+            result.append(matrix[_][c_start])
+
+        # Increment column index
+        c_start += 1
+
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/misc/test_misc.py
@@ -0,0 +1,79 @@
+import unittest
+
+from misc.print_matrix_spiral import matrix_spiral
+
+
+class TestMatrixSpiral(unittest.TestCase):
+
+    def test_basic_matrix_empty(self):
+        matrix = [[]]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = []
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_1x1(self):
+        matrix = [[1]]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1]
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_2x2(self):
+        matrix = [
+            [1, 2],
+            [3, 4]
+        ]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1, 2, 4, 3]
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_3x3(self):
+
+        matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1, 2, 3, 6, 9, 8, 7, 4, 5]
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_4x4(self):
+
+        matrix = [
+            [1,  2,  3,  4],
+            [5,  6,  7,  8],
+            [9,  10, 11, 12],
+            [13, 14, 15, 16]
+        ]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10]
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_2x4(self):
+
+        matrix = [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1, 2, 3, 4, 8, 7, 6, 5]
+        self.assertEqual(spiral_path, expected_path)
+
+    def test_basic_matrix_spiral_4x2(self):
+
+        matrix = [
+            [1, 2],
+            [3, 4],
+            [5, 6],
+            [7, 8]
+        ]
+
+        spiral_path = matrix_spiral(matrix)
+        expected_path = [1, 2, 4, 6, 8, 7, 5, 3]
+        self.assertEqual(spiral_path, expected_path)
