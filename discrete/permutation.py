c0e4ec221007307c6975f4ead3bfccc8c18ac3d3
Alvin <alvinchang2@gmail.com>
2017-12-05 12:15:38 -0800

    Add another implementation of string permutations with recursion and backtracking, refactor some permutation tests to include this new impl.

--- discrete/permutation.py
+++ discrete/permutation.py
@@ -1,6 +1,6 @@
 
 
-def generate_permutations_string(input_string, char_index=0):
+def generate_permutations_string1(input_string, char_index=0):
     """
     Generates permutations of a string by performing inversions left to right.
 
@@ -22,12 +22,52 @@ def generate_permutations_string(input_string, char_index=0):
         input_string_list = list(input_string)
         input_string_list[_i], input_string_list[char_index] = input_string_list[char_index], input_string_list[_i]
 
-        results.extend(generate_permutations_string(input_string_list, char_index=char_index + 1))
+        results.extend(generate_permutations_string1(input_string_list, char_index=char_index + 1))
     return results
 
 
-def generate_permutations_list():
-    pass
+def generate_permutations_string2(input_string):
+    """
+    Generates permutations via recursion and backtracking
+
+    """
+    results = generate_permutations_string2_helper(input_string, 0, len(input_string)-1)
+    return results
+
+
+def swap_string_index(s, idx1, idx2):
+    """
+    Swaps a string index by turning it into a list, and then swapping the elements at idx1, idx2. This is done
+    because strings are immutable and do not support item assignment.
+
+    :param s:
+    :type s: str
+
+    :param idx1:
+    :type idx1: int
+
+    :param idx2:
+    :type
+    :return:
+    """
+    str_list = list(s)
+    str_list[idx1], str_list[idx2] = str_list[idx2], str_list[idx1]
+    return "".join(str_list)
+
+
+def generate_permutations_string2_helper(input_string, lower, upper):
+    results = []
+
+    if lower == upper:
+        results.append(input_string)
+
+    for _i in xrange(lower, upper+1):
+        input_string = swap_string_index(input_string, _i, lower)
+        results.extend(generate_permutations_string2_helper(input_string, lower+1, upper))
+        # backtrack
+        input_string = swap_string_index(input_string, _i, lower)
+    return results
+
 
 
 
--- test/discrete/test_permutations.py
+++ test/discrete/test_permutations.py
@@ -1,20 +1,25 @@
 import unittest
 from itertools import permutations
 
-from discrete.permutation import generate_permutations_string
+from discrete.permutation import generate_permutations_string1, generate_permutations_string2
 
 
 class TestPermutations(unittest.TestCase):
 
-    def test_permutations_basic(self):
+    def test_all_permutations(self):
+        for permute_string_method in (generate_permutations_string1, generate_permutations_string2):
+            self.permutations_basic(permute_string_method)
+            self.permutations_empty(permute_string_method)
+
+    def permutations_basic(self, permute_string_method):
         basic_string = "abcdef"
-        actual_perms = set(generate_permutations_string(basic_string))
+        actual_perms = set(permute_string_method(basic_string))
         expected_perms = set("".join(_p) for _p in permutations(basic_string))
         self.assertTrue(actual_perms == expected_perms)
 
-    def test_permutations_empty(self):
+    def permutations_empty(self, permute_string_method):
         empty_string = " "
-        actual_perms = set(generate_permutations_string(empty_string))
+        actual_perms = set(permute_string_method(empty_string))
         expected_perms = set("".join(_p) for _p in permutations(empty_string))
         self.assertTrue(actual_perms == expected_perms)
 
