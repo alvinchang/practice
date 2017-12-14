3892f490c5728ea16f32fdc2f4a1a6ff75addf25
alvinchang <alvinchang2@gmail.com>
2017-12-13 16:48:41 -0800

    clean up code for find words and add some tests.

--- discrete/find_words.py
+++ discrete/find_words.py
@@ -1,50 +1,74 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
 
-
-example_string = "helloworld!"
-example_dict_list = ['he', 'hell', 'low', 'hello', 'world', '!']
-
-
-# - Hello , World, ! # 3 words
-
-# Don't want , hel lo world !
-
-# goodweather
 
 def find_min_words(string, dictionary):
-    # if string or dict is empty, no valid words.
+    """
+    Finds the minimum number of substrings in a string (no whitespaces) where each substring is in the dictionary.
+
+    :param string:
+    :param dictionary:
+    :return:
+    """
+    # If string or dict is empty, no valid words.
     if not string or not dictionary:
         return []
-    results = find_min_words_helper(string, dictionary, 0, len(string) - 1)
+    results = find_min_words_helper(string, set(dictionary), 0, len(string) - 1, [])
+
+    # Find the minimum amount of words
     min_result = min(results, key=lambda x: len(x))
     return min_result
 
 
-def find_min_words_helper(string, dictionary, lower, end, current_word_list=[]):
+def find_min_words_helper(string, dictionary, lower, end, current_word_list):
+    """
+
+    :param string:
+    :param dictionary:
+    :param lower:
+    :param end:
+    :param current_word_list:
+    :return:
+    """
     results = []
 
+    # Reached past the end, return current word list.
     if lower > end:
         return [current_word_list]
 
+    # Iterate over the string from the lower starting point, up until the end.
     for _i in xrange(lower, end + 1):
+
+        # the substring in consideration
         substring = string[lower:_i + 1]
+
+        # If the substring is found in the dictionary, add the substring to the current list of words
+        # and set the lower index to be the index after the upper index of the valid word
         if substring in dictionary:
-            # print "found word={}, lower={}, upper={}".format(substring, lower, _i)
-            # Found a word, lets save it for later.
             current_word_list_copy = current_word_list[:]
             current_word_list_copy.append(substring)
-            result = find_min_words_helper(string, dictionary, lower=_i + 1, end=end,
+            result = find_min_words_helper(string, dictionary,
+                                           lower=_i+1,
+                                           end=end,
                                            current_word_list=current_word_list_copy)
             results.extend(result)
 
     return results
 
 
-# example_string = "goodweather"
-# example_dict_list = ['good', 'weather', 'we', 'at', 'her']
+if __name__ == "__main__":
+    example_string = "goodweather"
+    example_dict_list = ['good', 'weather', 'we', 'at', 'her']
+    actual = find_min_words(example_string, example_dict_list)
+    # Note in this case we dont want, 'good', 'we', 'at', 'her' since its longer
+    expected = ['good', 'weather']
+    assert actual == expected
 
-# print 'hey'
-print find_min_words(example_string, example_dict_list)
+    example_string = "helloworld!"
+    example_dict_list = ['he', 'hell', 'low', 'hello', 'world', '!']
+    actual = find_min_words(example_string, example_dict_list)
+    expected = ['hello', 'world', '!']
+    assert actual == expected
+
+
 
 
 
